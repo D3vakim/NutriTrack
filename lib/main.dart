9bc81545216213dc6efbf5a6426f8de76bfd2708
Nayell998 <mathiassantosdassilva@gmail.com>
2026-05-18 14:49:28 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,9 @@ Observação: como já definiu a cor primária, está certo, mas poderia usar ap
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           ),
         ),
+        //cometário Botões padronizados, cor e formato definidos
+ Sugestão: adicionar também estilo para OutlinedButton e TextButton, para todos os botões seguirem o mesmo padrão
+        
 
         cardTheme: CardThemeData(
           elevation: 0,
