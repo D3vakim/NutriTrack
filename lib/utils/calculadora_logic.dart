79d05e62094e7bc108f61d121c901cf0be12705e
Nayell998 <mathiassantosdassilva@gmail.com>
2026-05-19 21:04:21 -0300

    Update calculadora_logic.dart

--- lib/utils/calculadora_logic.dart
+++ lib/utils/calculadora_logic.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // Função principal para cálculo do IMC — nome claro e objetivo
 Melhoria: adicionar validação para evitar altura ou peso <= 0 (evita erro ou resultado infinito)
 //Fórmula correta do cálculo de IMC
+// Fórmula correta do cálculo de IMC
   
 String calcularImc(double weight, double height) {
   double imc = weight / (height * height);
